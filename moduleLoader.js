af16786dea68241b00618e42106f4f91ee6e4ac1
Mike McNeil <michael.r.mcneil@gmail.com>
2013-01-06 04:29:51 -0600

    Doc adjustments + version bump.

--- moduleLoader.js
+++ moduleLoader.js
@@ -10,21 +10,33 @@ module.exports = {
 		options.optional = true;
 		return buildDictionary(options);
 	}
+
+	// federated: function(options) {
+	// 	options.federated = true;
+	// 	return buildDictionary(options);
+	// }
 };
 
-// buildDictionary ()
-// Go through each object, include the code, and determine its identity.
-// Tolerates non-existent files/directories by ignoring them.
-//
-// @dirname		:: the path to the source directory
-// @filter		:: the filter regex
-// $replaceExpr	:: the replace regex
-// @optional	:: if optional, don't throw an error if nothing is found
-// @fullPathKeyNames	:: whether to use the full path as the key name (instead of just the filename)
+/**
+buildDictionary ()
+
+Go through each object, include the code, and determine its identity.
+Tolerates non-existent files/directories by ignoring them.
+
+options {}
+	dirname			:: the path to the source directory
+	filter			:: only include modules whose filename matches this regex
+	pathFilter		:: only include modules whose full relative path matches this regex (relative from the entry point directory)
+	replaceExpr		:: in identity: use this regex to remove things like 'Controller' or 'Service' and replace them with replaceVal
+	replaceVal		:: in identity: see above (default value === '')
+	optional		:: if optional, don't throw an error if nothing is found
+	federated		:: if federated, build the module by grouping submodules by their immediate parent directory name
+*/
 function buildDictionary(options) {
 	
 	var files = require('include-all')(options);
 	var objects = {};
+
 	_.each(files, function(module, filename) {
 		
 		// If no 'identity' attribute was provided, 
